73f7ba557896e81d9f50fa4edf4ba7a509945464
tdora28 <tokaidora88@gmail.com>
2023-12-10 13:10:49 +0200

    Draw init snake body

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Main assets
 const board = document.querySelector('#board');
 const SIZE = 20;
+let snake;
 
 function setBoard() {
   for (let i = 0; i < SIZE * SIZE; i++) {
@@ -11,4 +12,23 @@ function setBoard() {
   board.style.gridTemplateRows = `repeat(${SIZE}, 1fr)`;
 }
 
+function initGame() {
+  snake = [{ x: 11, y: 11 }];
+  draw();
+}
+
+function draw() {
+  const boardDivs = document.querySelectorAll('#board div');
+  boardDivs.forEach((div) => {
+    div.classList.remove('snake');
+  });
+  snake.forEach((snakePart) => {
+    const partPosition = (snakePart.y - 1) * SIZE + (snakePart.x - 1);
+    board.children[partPosition].classList.add('snake');
+  });
+}
+
 setBoard();
+initGame();
+
+console.log(board.children[0]);
